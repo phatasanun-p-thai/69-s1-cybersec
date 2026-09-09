c2980a2ae236e282d6f8a977d5945221775c932f
phatsanun-p-thai <phatasanun-p@rmutp.ac.th>
2026-09-09 09:40:25 +0700

    update all file

--- src/api/mapping/services/mapping.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mapping service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::mapping.mapping');
--- src/api/student/content-types/student/lifecycles.js
+++
@@ -1,8 +0,0 @@
-const md5 = require('md5')
-module.exports = {
-    async beforeCreate(event) {
-        console.log('beforeCreate is work!', event.params.data)
-        // event.params.data.mobile = 'yxz'
-        event.params.data.mobile = md5(event.params.data.mobile)
-    }
-}
